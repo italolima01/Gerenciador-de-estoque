23d5838091aa26b0a3800bfab48eb0e7051a6f69
Ítalo Lima <italolimac01@gmail.com>
2025-08-01 14:43:15 +0000

    remova do calendário a opção de ano, e deixe o mês como slide e setas do

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -4,11 +4,10 @@
 import * as React from "react"
 import { ptBR } from 'date-fns/locale'
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, DropdownProps } from "react-day-picker"
+import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { ScrollArea } from './scroll-area'
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -28,7 +27,6 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
-        caption_dropdowns: "flex gap-2 [&_.rdp-vhidden]:hidden",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -59,30 +57,6 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Dropdown: ({ value, onChange, children, ...props }: DropdownProps) => {
-          const options = React.Children.toArray(
-            children
-          ) as React.ReactElement<React.HTMLProps<HTMLOptionElement>>[]
-          const selected = options.find((child) => child.props.value === value)
-          const handleChange = (value: string) => {
-            const changeEvent = {
-              target: { value },
-            } as React.ChangeEvent<HTMLSelectElement>
-            onChange?.(changeEvent)
-          }
-          return (
-            <select
-              value={value}
-              onChange={(e) => handleChange(e.target.value)}
-              className={cn(
-                buttonVariants({ variant: 'ghost' }),
-                'h-auto p-1 pr-6 text-sm font-medium'
-              )}
-            >
-              {options}
-            </select>
-          )
-        },
         IconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
@@ -97,3 +71,4 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
+
